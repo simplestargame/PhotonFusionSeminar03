1fdcb4b2b222cd70457dd54b690f3a8912743621
simplestar <>
2023-02-19 18:56:37 +0900

    refactor: space tab

--- Assets/SimplestarGame/Network/Scripts/Runner/NetworkGameManager.cs
+++ Assets/SimplestarGame/Network/Scripts/Runner/NetworkGameManager.cs
@@ -7,40 +7,40 @@ namespace SimplestarGame
     [RequireComponent(typeof(NetworkRunner))]
     public class NetworkGameManager : SimulationBehaviour, IPlayerJoined, IPlayerLeft
     {
-		[SerializeField] NetworkGame networkGame;
-		[SerializeField] NetworkPlayer networkPlayer;
+        [SerializeField] NetworkGame networkGame;
+        [SerializeField] NetworkPlayer networkPlayer;
 
-		void IPlayerJoined.PlayerJoined(PlayerRef playerRef)
-		{
-			if (!this.Runner.IsServer)
+        void IPlayerJoined.PlayerJoined(PlayerRef playerRef)
+        {
+            if (!this.Runner.IsServer)
             {
-				return;
-			}
-			if (!this.networkGameSpawned)
-			{
-				this.Runner.Spawn(this.networkGame);
-				this.networkGameSpawned = true;
-			}
-			var networkPlayer = this.Runner.Spawn(this.networkPlayer, inputAuthority: playerRef); 
-			this.networkPlayers.Add(playerRef, networkPlayer);
-			this.Runner.SetPlayerObject(playerRef, networkPlayer.Object);
-		}
-
-		void IPlayerLeft.PlayerLeft(PlayerRef playerRef)
-		{
-			if (!this.Runner.IsServer)
-            {
-				return;
+                return;
             }
-			if (!this.networkPlayers.TryGetValue(playerRef, out NetworkPlayer networkPlayer))
+            if (!this.networkGameSpawned)
             {
-				return;
+                this.Runner.Spawn(this.networkGame);
+                this.networkGameSpawned = true;
             }
-			this.Runner.Despawn(networkPlayer.Object);
-			this.networkPlayers.Remove(playerRef);
-		}
+            var networkPlayer = this.Runner.Spawn(this.networkPlayer, inputAuthority: playerRef); 
+            this.networkPlayers.Add(playerRef, networkPlayer);
+            this.Runner.SetPlayerObject(playerRef, networkPlayer.Object);
+        }
 
-		Dictionary<PlayerRef, NetworkPlayer> networkPlayers = new Dictionary<PlayerRef, NetworkPlayer>(200);
-		bool networkGameSpawned = false;
-	}
+        void IPlayerLeft.PlayerLeft(PlayerRef playerRef)
+        {
+            if (!this.Runner.IsServer)
+            {
+                return;
+            }
+            if (!this.networkPlayers.TryGetValue(playerRef, out NetworkPlayer networkPlayer))
+            {
+                return;
+            }
+            this.Runner.Despawn(networkPlayer.Object);
+            this.networkPlayers.Remove(playerRef);
+        }
+
+        Dictionary<PlayerRef, NetworkPlayer> networkPlayers = new Dictionary<PlayerRef, NetworkPlayer>(200);
+        bool networkGameSpawned = false;
+    }
 }
